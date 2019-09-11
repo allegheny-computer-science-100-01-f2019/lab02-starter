a987fc79968286d141a3f64277d67543a001d538
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-09-11 13:37:05 -0400

    Starter files for lab 02

---
+++ src/main/java/labtwo/DisplayArtwork.java
@@ -0,0 +1,35 @@
+package labtwo;
+
+import java.util.Date;
+import java.util.Scanner;
+
+/** This class has a template that displays ASCII artwork.
+ * The first line displays the name of the programmer and the date. Then, the
+ * program's main method displays nine lines of artwork that use characters. The
+ * program should produce the ten total lines of output by using only a total of
+ * nine println statements.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing source code
+ *
+ * @author Add Your Name Here
+ */
+
+public class DisplayArtwork {
+
+  /** The main entry point for DisplayArtwork.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display artwork
+   *  in the output, following the aforementioned requirements.
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+    // TODO: display the date using the Date constructor
+    System.out.println("Add Your Name Here " + new Date());
+    // TODO: display ten lines of the artwork
+    // while using println and print statements and "\n" escape character
+
+  }
+
+}
